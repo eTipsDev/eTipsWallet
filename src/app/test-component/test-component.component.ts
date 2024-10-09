9b084a50facb4ccfb11be10a49e8b965c7227581
SfisoNxumalo <sfisonxumalo69@gmail.com>
2024-10-09 11:45:52 +0200

    try 77

--- src/app/test-component/test-component.component.ts
+++ src/app/test-component/test-component.component.ts
@@ -7,53 +7,24 @@ import { RealtimeDBService } from '../../Services/Firebase/FirebaseDB/realtime-d
   styleUrl: './test-component.component.css'
 })
 export class TestComponentComponent implements OnInit{
-
-  loading:boolean = true;
-
   constructor(private realtime:RealtimeDBService){}
 
   userData:any ={
-    firstName:"",
-    lastName:"",
-    kyc:false
+    firstName:"...",
+    lastName:"........"
   }
 
-  visible:boolean = true
   ngOnInit(): void {
-    // setTimeout(() => {
-    // const ctx:any = document.getElementById('myChart');
-
-    //     new Chart(ctx, {
-    //       type: 'bar',
-    //       data: {
-    //         labels: ['Jan', '', 'Mar', '', 'May', ''],
-    //         datasets: [{
-    //           label: 'Tips Review',
-    //           data: [12, 19, 3, 5, 2, 3],
-    //           borderWidth: 1,
-    //           backgroundColor: 'orange',
-    //         }]
-    //       },
-    //       options: {
-    //         scales: {
-    //           y: {
-    //             beginAtZero: true
-    //           }
-    //         }
-    //       }
-    //     });
-    //     this.visible = false
-    // },  2000)
-
-    // setTimeout(()=> {
-    //   this.realtime.getLoggedUserDetails(this.realtime.mGetLoggedInUser().uid).then((data) => {
-    //     if(data){
-    //       this.userData = data
-    //     }
+    setTimeout(()=>{
+      this.realtime.getLoggedUserDetails(this.realtime.mGetLoggedInUser().uid).then((data) => {
+        if(data)
+        {
+          this.userData = data
+        }
         
-    //     this.loading = false
-    //   });
-    // },100)
+      });
+    },100)
+    
   }
 
 }
