dfa59b005c6ef2658a15570bb591149c5defebff
SfisoMawila <sfiso@thedigitalacademy.co.za>
2024-12-08 21:31:35 +0200

    updated UI

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,14 +2,7 @@ export const environment = {
 
 
   firebase:{
-        apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+      
   },
   ENDPOINT:""
 }
