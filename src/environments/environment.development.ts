7637efceb6e86a580cf17b66d1b94475250aaed4
SfisoMawila <sfiso@thedigitalacademy.co.za>
2024-12-08 21:33:59 +0200

    deployment5

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,8 +2,26 @@ export const environment = {
 
 
   firebase:{
-      
+    // apiKey: "AIzaSyALp-aODp9Sw017MLkSuTHqrvTCVL2sOM0",
+    // authDomain: "etipswallet-7442d.firebaseapp.com",
+    // databaseURL: "https://etipswallet-7442d-default-rtdb.firebaseio.com",
+    // projectId: "etipswallet-7442d",
+    // storageBucket: "etipswallet-7442d.appspot.com",
+    // messagingSenderId: "47042432896",
+    // appId: "1:47042432896:web:afa97bf95282c7fa54fb65",
+    // measurementId: "G-95VLWNWBGN"
+    
+        apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   },
   ENDPOINT:""
+  // ENDPOINT:"https://e-tips-backend.vercel.app/api/v1/",
+  // ENDPOINT:"http://localhost:3000/api/v1/"
 }
 
