82f299cf411f8d47e7686a83f62e1f8855acbb27
SfisoMawila <sfisonxumalo69@gmail.com>
2024-12-18 21:10:10 +0200

    updated 1

--- mynode.js
+++ mynode.js
@@ -1,8 +1,10 @@
+const dotenv = require('dotenv').config({path: 'src/.env'}); ;
+
 const fs = require('fs');
 const path = require('path');
 const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
-const dotenv = require('dotenv').config({path: 'src/.env'}); ;
+
 
 const envFile = `export const environment = {
 
